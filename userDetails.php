a96d9de6953c9a18656e89585ba80dfb14b7b428
azeem <azeemofficial10gmail.com>
2021-06-14 11:52:34 +0530

    Storing User Details with Cart Order functionality Done Successfully

--- userDetails.php
+++ userDetails.php
@@ -11,6 +11,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="../library/css/bootstrap.min.css">
 <style>
+.icon-container {
+  margin-bottom: 20px;
+  padding: 7px 0;
+  font-size: 24px;
+}
+
   h2, h3 {
     margin-top: 0px;
     padding-top: 0px;
@@ -18,19 +24,75 @@
   td {
     border-top: none !important;
   }
+  hr {
+  border: 1px solid lightgrey;
+}
 </style>
 </head>
 <body>
 
+ <?php
+
+$errorname = '';
+$erroremail = '';
+$errormobile = '';
+$erroraddress = '';
+$errorcity = '';
+$errorzip_code = '';
+$errorcountry = '';
+
+if(isset($_POST['submit'])){
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $mobile = $_POST['mobile'];
+    $address = $_POST['address'];
+    $city = $_POST['city'];
+    $zip_code = $_POST['zip_code'];
+    $country = $_POST['country'];
+
+    if(empty($name)){
+        $errorname .= "First Name Is Required";
+    }
+    if(empty($email)){
+        $erroremail .= "Email is Required";
+    }
+    if(empty($mobile)){
+      $errormobile .= "Number Is Required";
+  }
+        if(empty($address)){
+            $erroraddress .= "Address Is Required";
+        }
+        if(empty($city)){
+          $errorcity .= "City Is Required";
+      }
+      if(empty($zip_code)){
+        $errorzip_code .= "Zip Code Is Required";
+    }
+    if(empty($country)){
+      $errorcountry .= "Country Is Required";
+  }    
+        $sql = "INSERT INTO user_details (name, email, mobile, address, city, zip_code, country)VALUES('$name', '$email', '$mobile',
+         '$address', '$city', '$zip_code', '$country')";
+        $result = mysqli_query($conn, $sql);
+        if($result === TRUE){
+           header("Location: payments.php");
+            
+           echo "<script>alert('Successfull!');</script>";
+        }   
+    }
+?> 
+
     <div class="row">
       <div class="col-md-7 well">
         <h3>Billing Address</h3>
+        <form action="payments.php" method="POST">
         <div class="form-group">
           <div class="input-group">
             <div class="input-group-addon addon-diff-color">
                 <span class="glyphicon glyphicon-user"></span>
             </div>
-            <input class="form-control" type="text" id="billing_name" name="billing_name" placeholder="Full Name" value="<?= $customer['name']; ?>">
+            <input class="form-control" type="text" name="name" placeholder="Full Name"><span style="color:red";><?php echo $errorname;?></span></i>
           </div>
         </div>
 
@@ -39,7 +101,7 @@
             <div class="input-group-addon addon-diff-color">
                 <span class="glyphicon glyphicon-envelope"></span>
             </div>
-            <input class="form-control" type="text" id="billing_email" name="billing_email" placeholder="example@gmail.com" value="<?= $customer['email']; ?>">
+            <input class="form-control" type="email" name="email" placeholder="example@gmail.com"><span style="color:red";><?php echo $erroemail;?></span></i>
           </div>
         </div>
 
@@ -48,7 +110,7 @@
             <div class="input-group-addon addon-diff-color">
                 <span class="glyphicon glyphicon-earphone"></span>
             </div>
-            <input class="form-control" type="text" id="billing_tel" name="billing_tel" placeholder="+91 0000 00000 0" value="<?= $customer['mobile']; ?>">
+            <input class="form-control" type="tel"  name="mobile" placeholder="+91 0000 00000 0"> <span style="color:red";><?php echo $errormobile;?></span></i>
           </div>
         </div>
 
@@ -57,7 +119,7 @@
             <div class="input-group-addon addon-diff-color">
                 <span class="glyphicon glyphicon-home"></span>
             </div>
-            <input class="form-control" type="text" id="billing_address" name="billing_address" placeholder="11, Abc road" value="<?= $customer['address']; ?>">
+            <input class="form-control" type="text" name="address" placeholder="11, Abc road"><span style="color:red";><?php echo $erroraddress;?></span></i>
           </div>
         </div>
 
@@ -66,56 +128,88 @@
             <div class="input-group-addon addon-diff-color">
                 <span class="glyphicon glyphicon-home"></span>
             </div>
-            <input class="form-control" type="text" id="billing_city" name="billing_city" placeholder="Hydearabad" value="Hyderabad">
+            <input class="form-control" type="text" name="city" placeholder="Hydearabad"><span style="color:red";><?php echo $errorcity;?></span></i>
           </div>
         </div>
+
+        <div class="row">
+          <div class="col-md-5">
+            <div class="form-group">
+              <div class="input-group">
+                <div class="input-group-addon addon-diff-color">
+                    <span class="glyphicon glyphicon-home"></span>
+                </div>
+                <input class="form-control" type="text" name="state" placeholder="Telangana"><span style="color:red";><?php echo $errorstate;?></span></i>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-5 col-md-offset-2">
             <div class="form-group">
               <div class="input-group">
                 <div class="input-group-addon addon-diff-color">
                     <span class="glyphicon glyphicon-map-marker"></span>
                 </div>
-                <input class="form-control" type="text" id="billing_zip" name="billing_zip" placeholder="400-000-3" value="400-000-3">
+                <input class="form-control" type="text" name="zip_code" placeholder="400-000-3"><span style="color:red";><?php echo $errorzip_code;?></span></i>
               </div>
             </div>
-    
+          </div> 
+        </div>
         <div class="form-group">
           <div class="input-group">
             <div class="input-group-addon addon-diff-color">
                 <span class="glyphicon glyphicon-home"></span>
             </div>
-            <input class="form-control" type="text" id="billing_country" name="billing_country" placeholder="India" value="India">
+            <input class="form-control" type="text" name="country" placeholder="India"><span style="color:red";><?php echo $errorcountry;?></span></i>
           </div>
         </div>
       </div>
       <div class="col-md-4 col-md-offset-1 well">
         <div class="text-right">
-          <h3>Your Order</h3>
-          <h4><span class="glyphicon glyphicon-shopping-cart"></span><sup id="itemCount"><?= $cartPrices['itemCount']; ?></sup></h4>
+                  <h3>Your Order</h3>
+                  <hr>
+
+          <!-- <h4><span class="glyphicon glyphicon-shopping-cart"></span><sup id="itemCount"><?php //= //$cartPrices['itemCount']; ?></sup></h4> -->
           <table class="table">
+          <thead>
+				<tr>
+			<td class="image">Product</td>
+			<td class="description">Description</td>
+			<td class="price">Price</td>
+			<td class="quantity">Quantity</td>
+			<td class="total">Total</td>
+						</tr>
+	</thead>
             <tbody>
               <?php 
-                foreach ($cart as $key => $product) { 
-               ?>    
+              foreach($_SESSION['cart'] as $product){
+                //  $cart = $product['mrp']*$product['qty'];
+                
+                ?>
                 <tr>
-                  <td align="right" width="85%">
-                    <a href="#"><?= $product['id']; ?></a>
-                  </td>
-                  <td width="15%">
-                    <strong><span>&#x20b9;</span><?= ($product['mrp']*$product['quantity']); ?></strong>
-                  </td>
-              </tr>
+                <td class="cart_product">
+                <img data-enlargeable style="cursor: zoom-in" src="images/Uploads/<?php echo $product['image']; ?>" width="40" height="40" alt="">
+                </td>
+                <td class="cart_description">
+                
+                <?php echo $product['short_description'];?></p>
+                </td>
+                <td class="cart_price">
+                <p>$<?php echo $product['mrp'];?></p>
+                </td>
+            <td class="cart_quantity">
+            <div class="cart_quantity_button">
+            <p class="cart_quantity_input"> <?php echo $product['qty'];?></p>
+            </td>
+            <td class="cart_total itotal">
+            <p class="cart_quantity_input"> <?php echo $product['mrp']*$product['qty'];?></p>
               <?php } ?>
             </tbody>
           </table>
-          <hr style="border: 1px dotted gray;">
-          <p>Total: <strong>
-              <span>$</span><?= $mrp['ctotal']; ?>
-            </strong>
-          </p>
-        </div>
+          <hr>
+          <td>Cart Total :<span id="cTotal"></span></td>
         <div class="text-right">
         <form method="POST" action="payments.php">
-          <input type="submit" value="Proceed to Mode Of Payment" class="btn btn-warning btn-block">
+          <input type="submit" name="submit" value="Proceed to Mode Of Payment" class="btn btn-warning btn-block">
         </div>
       </div>
     </div>
@@ -125,3 +219,23 @@
 </html>
 
 <?php include 'footer.php';?>
+
+<script>
+	var iprice=document.getElementsByClassName('iprice');
+	var iquantity=document.getElementsByClassName('iquantity');
+	var itotal=document.getElementsByClassName('itotal');
+	var cTotal=document.getElementById('cTotal');
+	var ct=0; 
+	
+	// CART TOTAL
+	function subTotal(){
+		ct=0;
+		for ( i = 0; i < iprice.length; i++) {
+			itotal[i].innerText = (iprice[i].value)*(iquantity[i].value);
+			ct = ct + (iprice[i].value)*(iquantity[i].value);
+		}
+		cTotal.innerText = ct;
+	} 
+
+	subTotal();
+</script>
